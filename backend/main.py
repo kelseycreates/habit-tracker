fcc75944fdaee69d01589161d28d2a73fe1cf2f4
Kelsey Porter <kelsey.creates95@gmail.com>
2026-03-24 17:02:50 -0600

    Test CORS fix

--- backend/main.py
+++ backend/main.py
@@ -11,11 +11,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:5173",
-        "http://127.0.0.1:5173",
-        "https://bejeweled-monstera-bc39e1.netlify.app",
-    ],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
